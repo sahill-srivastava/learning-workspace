6a0199f925b3f9a6af02ca0a6c0dc520400b566a
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-05-27 10:22:49 +0530

    learn implementing state

--- react/class-based-react/src/AppClass.jsx
+++ react/class-based-react/src/AppClass.jsx
@@ -1,16 +1,24 @@
 import { Component } from "react";
 
 class AppClass extends Component {
-//   constructor(props) {
-//     super(props);
+  constructor(props) {
+    super(props);
 
-//     console.log(props);
-//   }
+    this.state = {
+        count: 0,
+    }
+  }
 
   render() {
+
+    const {name} = this.props;
+    const {count} = this.state;
+
+
     return <div>
         <h1>Class Based Component</h1>
-        <div>{this.props.name}</div>
+        <div>{name}</div>
+        <div>{count}</div>
     </div>;
   }
 }
