c8ecb717e15e612f5d6b23c9c107ec52af82ebc7
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-05-27 09:10:08 +0530

    learn class based component

---
+++ react/class-based-react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Normal funcitonal component
+    </div>
+  )
+}
+
+export default App
---
+++ react/class-based-react/src/AppClass.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react"
+
+class AppClass extends Component {
+
+
+  render() {
+    return (
+      <div>Class Based Component</div>
+    )
+  }
+}
+
+export default AppClass;
