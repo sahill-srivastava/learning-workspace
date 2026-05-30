9c8b5091ff7ef98d4cc86c9fa8a404773fce8e5b
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-05-30 08:59:00 +0530

    fix online status logic

--- react/04_custom_hooks/src/hooks/useOnlineStatus.jsx
+++ react/04_custom_hooks/src/hooks/useOnlineStatus.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 
 const useOnlineStatus = () => {
-  const [onlineStatus, setOnlineStatus] = useState(true);
+  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine);
 
   useEffect(() => {
+    console.log("useeffect")
     window.addEventListener("offline", () => {
       console.log("disconnected");
       setOnlineStatus(false);
     });
+
+    window.addEventListener("online", () => {
+      console.log("connected");
+      setOnlineStatus(true);
+    });
+
+    
   }, []);
 
+  console.log("onlineStatus", onlineStatus)
   return onlineStatus;
 };
 
