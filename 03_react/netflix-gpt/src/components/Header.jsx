b27cd586208fe4f8ce2cd1b9b28218d35e0ed563
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-26 09:05:53 +0530

    adding form

--- 03_react/netflix-gpt/src/components/Header.jsx
+++ 03_react/netflix-gpt/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/netflix-logo.png";
 
 const Header = () => {
   return (
-    <div>
+    <div className="absolute z-50 px-5 py-2.5 bg-gradient-to-b from-black">
          <img className="w-40" src={logo} alt="logo" />
     </div>
   )
--- 03_react/netflix-gpt/src/components/UserLogin.jsx
+++ 03_react/netflix-gpt/src/components/UserLogin.jsx
@@ -5,8 +5,15 @@ const UserLogin = () => {
   return (
     <div>
         <Header />
-       <main>
-         <img src={heroBanner} alt="banner" />
+       <main className="relative">
+        <div>
+           <img className="w-screen  object-cover" src={heroBanner} alt="banner" />
+        </div>
+        <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-2 w-3/12 bg-black flex flex-col items-center justify-center">
+          <input type="text" placeholder="Email Address" className="p-2  m-2"/>
+          <input type="password" placeholder="Email Password" className="p-2  m-2"/>
+          <button className="p-4 m-4 ">Sign In</button>
+        </form>
        </main>
     </div> 
   )
