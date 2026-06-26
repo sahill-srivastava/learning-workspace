7927d5bcde31c63aea9c4b147143366d39411dfd
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-26 09:49:56 +0530

    add sign up

--- 03_react/netflix-gpt/src/components/UserLogin.jsx
+++ 03_react/netflix-gpt/src/components/UserLogin.jsx
@@ -1,22 +1,48 @@
-import Header from "./Header"
-import heroBanner from "../assets/netflix-hero-banner.jpg"
+import Header from "./Header";
+import heroBanner from "../assets/netflix-hero-banner.jpg";
+import { useState } from "react";
 
 const UserLogin = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div>
-        <Header />
-       <main className="relative">
-        <div>
-           <img className="w-screen  object-cover" src={heroBanner} alt="banner" />
+      <Header />
+      <main className="relative">
+        <div className="relative">
+          <img
+            className="w-screen h-screen  object-cover"
+            src={heroBanner}
+            alt="banner"
+          />
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50"></div>
         </div>
-        <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-2 w-3/12 bg-black flex flex-col items-center justify-center">
-          <input type="text" placeholder="Email Address" className="p-2  m-2"/>
-          <input type="password" placeholder="Email Password" className="p-2  m-2"/>
-          <button className="p-4 m-4 ">Sign In</button>
+        <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 py-20 px-10 rounded-2xl w-3/12 bg-black/75 text-white flex flex-col items-start justify-center">
+          <h1 className="font-bold text-2xl">{isSignIn ? "Sign In" : "Sign Up"}</h1>
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="w-full bg-zinc-800 p-4  mt-8 rounded outline-1 outline-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full bg-zinc-800 p-4  mt-8 rounded outline-1 outline-white"  
+          />
+          <button className="w-full p-4 my-10 rounded bg-red-600">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </button>
+          <p onClick={toggleSignInForm} className="cursor-pointer">
+            New to Netflix? Sign Up Now
+          </p>
         </form>
-       </main>
-    </div> 
-  )
-}
+      </main>
+    </div>
+  );
+};
 
-export default UserLogin
+export default UserLogin;
