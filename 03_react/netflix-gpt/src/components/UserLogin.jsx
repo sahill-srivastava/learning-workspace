7b2d719ebbc493c876ecd477646f84f417e0fcf5
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-06-27 08:33:12 +0530

    fix something

--- 03_react/netflix-gpt/src/components/UserLogin.jsx
+++ 03_react/netflix-gpt/src/components/UserLogin.jsx
@@ -22,7 +22,9 @@ const UserLogin = () => {
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50"></div>
         </div>
         <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 py-20 px-10 rounded-2xl w-3/12 bg-black/75 text-white flex flex-col items-start justify-center">
-          <h1 className="font-bold text-2xl">{isSignIn ? "Sign In" : "Sign Up"}</h1>
+          <h1 className="font-bold text-2xl">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
           <input
             type="text"
             placeholder="Full Name"
@@ -38,13 +40,15 @@ const UserLogin = () => {
           <input
             type="password"
             placeholder="Password"
-            className="w-full bg-zinc-800 p-4  mt-8 rounded outline-1 outline-white"  
+            className="w-full bg-zinc-800 p-4  mt-8 rounded outline-1 outline-white"
           />
           <button className="w-full p-4 my-10 rounded bg-red-600">
             {isSignIn ? "Sign In" : "Sign Up"}
           </button>
           <p onClick={toggleSignInForm} className="cursor-pointer">
-            {isSignIn ? "New to Netflix? Sign Up Now" : "Already User? Sign In Now"}
+            {isSignIn
+              ? "New to Netflix? Sign Up Now"
+              : "Already User? Sign In Now"}
           </p>
         </form>
       </main>
