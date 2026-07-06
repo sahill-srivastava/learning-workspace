80e5dd875cd186949ec90efa329351161ac575b0
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-06 19:10:06 +0530

    add movies store

--- 03_react/netflix-gpt/src/components/Browse.jsx
+++ 03_react/netflix-gpt/src/components/Browse.jsx
@@ -1,8 +1,13 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  //Fetch Data from TMDB API and update store
+  const dispatch = useDispatch();
+
   const getNowPlayingMovies = async () => {
     const res = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -13,7 +18,9 @@ const Browse = () => {
 
     const data = await res.json();
 
-    console.log("data: ", data);
+    console.log("data: ", data.results);
+
+    dispatch(addNowPlayingMovies(data.results))
   };
 
   useEffect(() => {
