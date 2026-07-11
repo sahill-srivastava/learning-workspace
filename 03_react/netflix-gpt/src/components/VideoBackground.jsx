dc577bf2ef60e7e902b77dbeaa3ca6f10c456102
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-11 11:04:13 +0530

    build top portion

--- 03_react/netflix-gpt/src/components/VideoBackground.jsx
+++ 03_react/netflix-gpt/src/components/VideoBackground.jsx
@@ -9,8 +9,8 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div className="">
       <iframe
-      className="w-screen aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=2xYMdilmVDqDDWeU`}
+        className="w-screen aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&playsinline=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- 03_react/netflix-gpt/src/components/VideoTitle.jsx
+++ 03_react/netflix-gpt/src/components/VideoTitle.jsx
@@ -4,10 +4,10 @@ const VideoTitle = ({ title, description }) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{description}</p>
       <div className="flex gap-4 items-center justify-start">
-        <button className="bg-white text-black text-xl px-6 py-2.5 rounded cursor-pointer flex items-center gap-2">
+        <button className="bg-white text-black hover:opacity-70 text-xl px-6 py-2.5 rounded cursor-pointer flex items-center gap-2">
          <span className="text-2xl">▶︎</span> Play
         </button>
-        <button className="bg-white/40 text-white text-xl px-6 py-2.5 rounded cursor-pointer flex items-center gap-2">
+        <button className="bg-white/40  hover:bg-white/20 text-white text-xl px-6 py-2.5 rounded cursor-pointer flex items-center gap-2">
          <span className="text-3xl">ⓘ</span> More Info
         </button>
       </div>
