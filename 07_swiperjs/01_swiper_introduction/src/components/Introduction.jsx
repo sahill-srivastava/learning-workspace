4bbe8afcb5212b44c5fddf8f7cfa198c51ada2f6
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-14 17:42:21 +0530

    refactor code

--- 07_swiperjs/01_swiper_introduction/src/components/Introduction.jsx
+++ 07_swiperjs/01_swiper_introduction/src/components/Introduction.jsx
@@ -1,7 +1,11 @@
 const Introduction = () => {
-  return <div>
-    <h1 className="text-red-500">Intro</h1>
-  </div>;
+  return (
+    <div>
+      <h1 className="text-red-500">Intro</h1>
+    </div>
+  );
 };
 
 export default Introduction;
+
+
