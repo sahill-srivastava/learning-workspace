29dbff21b50d31c43aa126c1427ed6e348931f27
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-18 18:33:00 +0530

    establishing connection with db

--- 06_node/01_introduction/database.js
+++ 06_node/01_introduction/database.js
@@ -1,3 +1,27 @@
-require("mongodb");
+// import { MongoClient } from 'mongodb'
+const { MongoClient } = require('mongodb');
 
-const URI = "mongodb+srv://sahil682002_db_user:1UptXTu9facd8TgB@namastenode.fvk5f8i.mongodb.net/"
+// Connection URL
+const url = "mongodb+srv://sahil682002_db_user:1UptXTu9facd8TgB@namastenode.fvk5f8i.mongodb.net/"
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'HelloWorld';
+
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
