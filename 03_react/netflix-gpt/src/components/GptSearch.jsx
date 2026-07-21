8b4d102ed008520588823c8855d8b849103a1680
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-21 11:44:29 +0530

    fix search bar

--- 03_react/netflix-gpt/src/components/GptSearch.jsx
+++ 03_react/netflix-gpt/src/components/GptSearch.jsx
@@ -1,13 +1,22 @@
-import GptMovieSuggestions from "./GptMovieSuggestions"
-import GptSearchBar from "./GptSearchBar"
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+import heroBanner from "../assets/netflix-hero-banner.jpg";
 
 const GptSearch = () => {
   return (
     <div>
+      <div className="relative">
+        <img
+          className="w-screen h-screen  object-cover"
+          src={heroBanner}
+          alt="banner"
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50"></div>
         <GptSearchBar />
-        <GptMovieSuggestions />
+      </div>
+      <GptMovieSuggestions />
     </div>
-  )
-}
+  );
+};
 
-export default GptSearch
+export default GptSearch;
--- 03_react/netflix-gpt/src/components/GptSearchBar.jsx
+++ 03_react/netflix-gpt/src/components/GptSearchBar.jsx
@@ -1,10 +1,10 @@
 
 const GptSearchBar = () => {
   return (
-    <div className="pt-50">
-        <form className="w-1/2 bg-black p-5">
-            <input type="text" placeholder="What would you like to watch today?" className="bg-white p-2 px-3 rounded mr-2"/>
-            <button className="text-white bg-red-600 rounded p-2 px-3 cursor-pointer">Search</button>
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-20 pt-40 flex justify-center">
+        <form className="w-1/2 h-[80px] bg-black p-5 rounded flex gap-2.5">
+            <input className="flex-1  bg-white p-2 px-3 rounded mr-2" type="text" placeholder="What would you like to watch today?" />
+            <button className="w-[20%] text-white bg-red-600 rounded py-2 px-5 cursor-pointer">Search</button>
         </form>
     </div>
   )
