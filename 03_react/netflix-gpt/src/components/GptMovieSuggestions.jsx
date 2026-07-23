7af34e220d49f43dbf16f56eb83490538d3754d6
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-23 20:52:43 +0530

    feat: implementing search bar

--- 03_react/netflix-gpt/src/components/GptMovieSuggestions.jsx
+++ 03_react/netflix-gpt/src/components/GptMovieSuggestions.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux";
+import MovieList from "./MovieList"
 
+/*-----------------------Refactor it----------------------------- */
 const GptMovieSuggestions = () => {
-  // const { movieNames, movieResults } = useSelector((store) => store.gpt);
+  const { movieNames, movieResults } = useSelector((store) => store.gpt);
 
-  // if (!movieNames) return null;
+  if (!movieNames) return null;
 
   return (
-    <div className="w-[600px]">
-      <p className=" bg-yellow-400 p-5 rounded flex gap-2.5">Hey sahil</p>
+    <div className="w-[600px] bg-yellow-400 p-5 rounded flex gap-2.5">
+      {
+        movieResults.map((movieName, index) => {
+          <MovieList 
+          key={movieName}
+          title={movieName}
+          movies={movieResults[index]}
+          />
+        })
+      }
     </div>
   );
 };
