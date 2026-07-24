bd01b22515502d958af9e93a3fb962aaa13b40b8
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-07-24 18:59:08 +0530

    learnt memoization

--- 03_react/netflix-gpt/src/hooks/useNowPlayingMovies.js
+++ 03_react/netflix-gpt/src/hooks/useNowPlayingMovies.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 
@@ -8,6 +8,8 @@ const useNowPlayingMovies = () => {
 
     //Fetch Data from TMDB API and update store
     const dispatch = useDispatch();
+
+    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies)
 
     const getNowPlayingMovies = async () => {
         const res = await fetch(
@@ -23,7 +25,7 @@ const useNowPlayingMovies = () => {
     };
 
     useEffect(() => {
-        getNowPlayingMovies();
+        if(!nowPlayingMovies) getNowPlayingMovies();
     }, []);
 
 }
