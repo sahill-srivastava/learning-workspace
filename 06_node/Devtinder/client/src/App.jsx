4e03f3e85f9d76240cc9659344e6f7f6d6ab7ecb
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-08-19 09:52:05 +0530

    setup user store

--- 06_node/Devtinder/client/src/App.jsx
+++ 06_node/Devtinder/client/src/App.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Profile from "./Profile";
 import Body from "./Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 };
