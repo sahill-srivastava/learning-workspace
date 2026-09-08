24da6b4ef61f3a10d391c7d85a40f13e900917b3
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-09-08 10:19:14 +0530

    learnt cron jobs

--- 06_node/Devtinder/server/src/app.js
+++ 06_node/Devtinder/server/src/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database")
 const app = express();
 const cookieParser = require("cookie-parser")
 const cors = require("cors");
+require("./utils/cronjob")
 
 
 //Middlewares
---
+++ 06_node/Devtinder/server/src/utils/cronjob.js
@@ -0,0 +1,35 @@
+const cron = require("node-cron")
+const { subDays, startOfDay, endOfDay, endOfDay } = require("date-fns");
+const ConnectionRequestModel = require("../models/connectionRequest")
+
+cron.schedule("0 8 * * *", () => {
+
+    try {
+
+        const yesterday = subDays(new Date(), 1)
+
+
+        const yesterdayStart = startOfDay(yesterday)
+        const yesterdayEnd = endOfDay(yesterday)
+
+        const pendingRequests = ConnectionRequestModel.find({
+            status: "interested",
+            createdAt: {
+                $gte: yesterdayStart,
+                $lt: yesterdayEnd
+            }
+        }).populate("fromUserId toUserId");
+
+        const listOfEmails = [...new Set(pendingRequests.map(req => req.toUserIds.emailId))];
+
+        for(const email of listOfEmails) {
+            // Send Emails
+            
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
