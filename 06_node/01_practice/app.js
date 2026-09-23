c960f242db8f054609d3ccff3906b6b9fcb77a57
Sahil Srivastava <sahilsrivastavawork@gmail.com>
2026-09-23 19:16:11 +0530

    learnt: sync vs async vs promise operations in fs module

--- 06_node/01_practice/app.js
+++ 06_node/01_practice/app.js
@@ -1,3 +1,17 @@
-import { builtinModules } from "node:module";
+// /promises is important for promised based api
+import fs from "node:fs/promises";
 
-console.log(builtinModules);
+console.log("1")
+
+//promise based readfile operation
+const fileReading = async () => {
+
+    const data = await fs.readFile("data.txt", "utf-8")
+
+    console.log(data)
+}
+
+fileReading();
+
+
+console.log("2")
